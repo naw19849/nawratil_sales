a936b7ee39b5e1c27ecaf899d822464b6bf13d28
Stefan <naw19849@spengergasse.at>
2020-04-24 17:26:14 +0200

    new class Customers ArrayList

---
+++ src/Customers.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class Customers {
+	private ArrayList<Customer> customersList;
+
+	public ArrayList<Customer> getCustomersList() {
+		return customersList;
+	}
+
+	public void setCustomersList(ArrayList<Customer> customersList) {
+		this.customersList = customersList;
+	}
+}
