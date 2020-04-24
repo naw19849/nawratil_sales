e85f2072b1b9efd993ff48e94e3fa67f14418d4d
Stefan <naw19849@spengergasse.at>
2020-04-24 17:32:18 +0200

    Customer credit added

--- src/Customer.java
+++ src/Customer.java
@@ -1,4 +1,26 @@
 
 public class Customer {
+	private int customerID;
+	private String firstName;
+	
+	public Customer(int customerID, String firstName) {
+		this.customerID = customerID;
+		this.firstName = firstName;
+	}
 
+	public int getCustomerID() {
+		return customerID;
+	}
+	
+	public void setCustomerID(int customerID) {
+		this.customerID = customerID;
+	}
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
 }
